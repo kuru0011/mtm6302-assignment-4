bcbbead2d0b00ccfe17fed76231cc05927c7b41b
Abhimanyu <kuru0011@algonquinlive.com>
2021-12-10 16:58:36 -0500

    Completes assignment

---
+++ script.js
@@ -0,0 +1,84 @@
+const $main = document.getElementById('main')
+
+// adding innerHTML in the container
+$main.innerHTML = `
+<div id = "hbMenu" class = "menu">
+    <button id='button' class="btn"><i class="fas fa-bars"></i></button>
+    <div id = 'links' class='links'>
+        <a href="#articleOne">Article One</a>
+        <a href="#articleTwo">Article Two</a>
+        <a href="#articleThree">Article Three</a>
+    </div>
+</div>
+
+<div id = "content">
+    <div id = "articleOne">
+        <h1>Article One</h1>
+        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        </p>
+    </div>
+    <div id = "articleTwo">
+        <h1>Article Two</h1>
+        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        </p>
+    </div>
+    <div id = "articleThree" class= "fade">
+        <h1>Article Three</h1>
+        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        </p>
+    </div>
+</div>`
+
+// targetting all elements through variable added dynamically
+const $hbMenu = document.getElementById('hbMenu')
+const $button = document.getElementById('button')
+const $links = document.getElementById('links')
+const $content = document.getElementById('content')
+const $articleOne = document.getElementById('articleOne')
+const $articleTwo = document.getElementById('articletwo')
+const $articleThree = document.getElementById('articleThree')
+
+// hiding menu 
+$links.style.display = 'none'
+
+// displaying the menu when button is clicked
+$button.addEventListener('click', function(){
+    if($links.style.display === 'none'){
+        $links.style.display = 'block'
+    }
+    else{
+        $links.style.display = 'none'
+    }
+})
+
+const articleThreeHeight = $articleThree.clientHeight;
+
+document.addEventListener('scroll', transitionTrigger);
+
+// function to check position of an element
+function checkPosition() {
+    const viewportHeight = window.innerHeight;
+    const scrollY = window.scrollY;
+    const scrollPosition = scrollY + viewportHeight;
+    const articleThreePosition = $articleThree.getBoundingClientRect().top;
+
+    console.log(scrollPosition, articleThreePosition)
+
+    if (scrollPosition > articleThreePosition) {
+        return true;
+    }
+    return false;
+}
+
+
+// function to toggle between fade-in fade-out classes to trigger animation on element 3
+function transitionTrigger() {
+    if (checkPosition()) {
+        $articleThree.classList.remove('hide');
+        $articleThree.classList.add('reveal');
+    }
+    else {
+        $articleThree.classList.remove('reveal');
+        $articleThree.classList.add('hide');
+    }
+}
